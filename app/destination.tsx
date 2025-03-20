129dca7cb1dc95bc73883e6bf1b40837ba033930
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-03-20 23:34:33 +0530

    add navigation to destination page through DestinationCard

---
+++ app/destination.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+const destination = () => {
+  return (
+    <View>
+      <Text>destination</Text>
+    </View>
+  )
+}
+export default destination
+const styles = StyleSheet.create({})
--- components/Destinations.tsx
+++ components/Destinations.tsx
@@ -1,21 +1,27 @@
 import { destinationData } from "@/constants";
 import { LinearGradient } from "expo-linear-gradient";
+import { useNavigation } from "expo-router";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const Destinations = () => {
+  const navigation = useNavigation();
+
   return (
     <View className="mx-7 flex-row justify-between flex-wrap">
       {destinationData.map((dest, index) => (
-        <DestinationCard dest={dest} key={index} />
+        <DestinationCard dest={dest} key={index} navigation={navigation} />
       ))}
     </View>
   );
 };
 export default Destinations;
 
-const DestinationCard = ({ dest }) => {
+const DestinationCard = ({ dest, navigation }) => {
   return (
-    <TouchableOpacity className="h-60 w-[12rem] relative flex justify-end p-4 py-6 space-y-2 mb-5">
+    <TouchableOpacity
+      onPress={() => navigation.navigate("/destination", { ...dest })}
+      className="h-60 w-[12rem] relative flex justify-end p-4 py-6 space-y-2 mb-5"
+    >
       <Image
         source={dest.image}
         className="absolute h-60 w-[12rem] rounded-3xl"
@@ -27,8 +33,12 @@ const DestinationCard = ({ dest }) => {
         end={{ x: 0.5, y: 1 }}
         style={{ borderBottomLeftRadius: 25, borderBottomRightRadius: 25 }}
       />
-      <Text className="text-[1.15rem] text-white font-bold mb-3">{dest.title}</Text>
-      <Text className="text-white text-[10px] leading-tight">{dest.shortDescription}</Text>
+      <Text className="text-[1.15rem] text-white font-bold mb-3">
+        {dest.title}
+      </Text>
+      <Text className="text-white text-[10px] leading-tight">
+        {dest.shortDescription}
+      </Text>
     </TouchableOpacity>
   );
 };
